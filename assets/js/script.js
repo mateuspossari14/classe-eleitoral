b1622f1670cc4bb71119053910fac69d61fd3c3f
mateuspossari14 <mateuspossari14@gmail.com>
2025-02-10 17:10:26 -0300

    adding maxLenght for the calculatorDisplay & adding the descriptionUsers function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 const calculatorDisplay = document.getElementById('calculator-display');
 
+calculatorDisplay.addEventListener('input', () => {
+    if (calculatorDisplay.value.length > 4)
+        calculatorDisplay.value = calculatorDisplay.value.slice(0, 4);
+});
+
 const insert = num => {
     let numberCalculator = calculatorDisplay.value;
 
@@ -11,8 +16,6 @@ const del = () => calculatorDisplay.value = '';
 
 const send = () => {
     let numberCalculator = parseInt(calculatorDisplay.value);
-    let imageDescription = window.document.querySelector('.video-container--description');
-    let video = window.document.getElementById('sourceVideo');
 
     let date = new Date;
     let actualYear = date.getFullYear();
@@ -31,45 +34,40 @@ const send = () => {
         else if (ageUser > 130)
             alert('Sua idade é muito avançada, tente novamente!');
 
-        else if (ageUser < 16) {
-            document.querySelector('.video-container').style.display = 'flex';
-            imageDescription.textContent = 'Sua classe eleitoral: não eleitor';
-            video.style.display = 'block';
-            video.src = './assets/videos/students.mp4';
-            video.load();
-            video.play();
-            refresh();
-        }
+        else if (ageUser < 16)
+            descriptionsUsers('não eleitor', 0);
 
-        else if (ageUser >= 16 && ageUser <= 17) {
-            document.querySelector('.video-container').style.display = 'flex';
-            imageDescription.textContent = 'Sua classe eleitoral: eleitor facultativo';
-            video.style.display = 'block';
-            video.src = './assets/videos/adolecents.mp4';
-            video.load();
-            video.play();
-            refresh();
-        }
+        else if (ageUser >= 16 && ageUser <= 17)
+            descriptionsUsers('eleitor facultativo', 1);
 
-        else if (ageUser >= 18 && ageUser <= 70) {
-            document.querySelector('.video-container').style.display = 'flex';
-            imageDescription.textContent = 'Sua classe eleitoral: eleitor obrigatório';
-            video.style.display = 'block';
-            video.src = './assets/videos/adult.mp4';
-            video.load();
-            video.play();
-            refresh();
-        }
+        else if (ageUser >= 18 && ageUser <= 70)
+            descriptionsUsers('eleitor obrigatório', 2);
 
-        else {
-            imageDescription.textContent = 'Sua classe eleitoral: eleitor facultativo';
-            video.style.display = 'block';
-            video.src = './assets/videos/old_people.mp4';
-            video.load();
-            video.play();
-            refresh();
-        };
+        else
+            descriptionsUsers('eleitor facultativo', 3);
     };
+
+    refresh();
 };
 
-let refresh = () => setInterval(() => location.reload(), 10000);
+let refresh = () => setTimeout(() => location.reload(), 10000);
+
+const descriptionsUsers = (electoralClass, indexVideo) => {
+    let arrayVideos = [
+        './assets/videos/students.mp4', 
+        './assets/videos/adolecents.mp4', 
+        './assets/videos/adult.mp4', 
+        './assets/videos/old_people.mp4'
+    ];
+
+    let video = window.document.getElementById('sourceVideo');
+    let imageDescription = window.document.querySelector('.video-container--description');
+
+    document.querySelector('.video-container').style.display = 'flex';
+    imageDescription.textContent = `Sua classe eleitoral: ${electoralClass}`;
+    video.style.display = 'block';
+    video.src = arrayVideos[indexVideo];
+
+    video.load();
+    video.play();
+};
